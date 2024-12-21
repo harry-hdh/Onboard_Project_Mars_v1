4132bc224694683b80dc4b14bb2ec9fced826870
harry-hdh <hdh997@outlook.com>
2024-12-21 15:07:43 +1300

    Adjust test cases for Language

--- SpecFlowProjectMars/Features/LanguagesFeature.feature
+++ SpecFlowProjectMars/Features/LanguagesFeature.feature
@@ -20,12 +20,12 @@ Scenario Outline: 02) Add new language with invalid data
 	Then I click on Add New button to activate form
 	And I enter invalid data as '<Language>' and select level as '<Level>' 
 	Then I try to click Add button
-	Then I should not be able to add new language
+	Then I should not be able to add new language with '<language>'
 	Examples: 
-	| Language | Level  |
-	| =+       | {}     |
-	| .?       | Basic  |
-	| ""       | Fluent |
+	| Language | Level            |
+	| =+       | Native/Bilingual |
+	| .?       | Basic            |
+	| ""       | Fluent           |
 
 @Add_Languages_Negative
 Scenario: 03) Add new language without enter data
@@ -45,21 +45,28 @@ Scenario Outline: 05) Edit existing language with invalid data
 	When I click on edit icon button to activate form
 	Then I update language '<Language>' and select level  '<Level>' 
 	Then I try click Update button
-	Then I should not be able to update language
+	Then I should not be able to update language with '<Language>'
 	Examples: 
-	| Language | Level  |
-	| ???      | test   |
-	| ---      | Basic  |
-	| ///      | Fluent |
+	| Language | Level            |
+	| ???      | Basic            |
+	| ---      | Native/Bilingual |
+	| ///      | Fluent           |
 
 @Edit_Languages_Negative
-Scenario: 06) Edit existing language without data
+Scenario: 06) Edit existing language with same data
 	When I click on edit icon button to activate language form
 	Then I try click Update button to update
 	Then I should not be able to update language and see warning message
 
+@Edit_Languages_Negative
+Scenario: 07) Edit existing language without data
+	When I click on edit icon to activate language form
+	Then I clear existing language and level
+	Then I try click Update to update record
+	Then should not allow to update language and see warning message
+
 @Delete_Languages
-Scenario: 07) Delete existing language
+Scenario: 08) Delete existing language
 	When I click on delete icon button
 	Then I should be able to delete language successfully
 
--- SpecFlowProjectMars/StepDefinitions/LanguagesStepDefinitions.cs
+++ SpecFlowProjectMars/StepDefinitions/LanguagesStepDefinitions.cs
@@ -1,6 +1,11 @@
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi.Modules.Log;
 using SpecFlowProjectMars.Drivers;
+using SpecFlowProjectMars.Utilities;
 using System;
+using System.Numerics;
 using TechTalk.SpecFlow;
 using Testing_Project_Mars_SpecFlow.Pages;
 
@@ -8,8 +13,16 @@ namespace SpecFlowProjectMars.StepDefinitions
 {
     [Binding]
     public class LanguagesStepDefinitions : CommonDriver
+
     {
         ProfilePage profilePageObj = new ProfilePage();
+
+        LanguageModel _languageModel;
+
+        public LanguagesStepDefinitions()
+        {
+            _languageModel = new LanguageModel();
+        }
 
         [Given(@"I navigate to languages sub-tab")]
         public void GivenINavigateToLanguagesSub_Tab()
@@ -21,7 +34,7 @@ namespace SpecFlowProjectMars.StepDefinitions
         [Given(@"I clean all existing data")]
         public void WhenICleanAllExistingData()
         {
-            profilePageObj.CleanRecords(_driver, "language row", "remove language btns");
+            profilePageObj.CleanRecords(_driver, "language table", "language rows", "remove language btns");
         }
 
         [Then(@"I click on Add New button")]
@@ -32,142 +45,206 @@ namespace SpecFlowProjectMars.StepDefinitions
         }
 
         [Then(@"I enter language as '([^']*)' and select level as '([^']*)'")]
-        public void ThenIEnterLanguageAsAndSelectLevelAs(string eng, string p1)
+        public void ThenIEnterLanguageAsAndSelectLevelAs(string language, string level)
         {
-            throw new PendingStepException();
+            _languageModel.Language = language;
+            _languageModel.Level = level;
+
+            profilePageObj.EnterTxtLangOrSkill(_driver, "language textbox", "level dropdown", _languageModel.Language, _languageModel.Level, "by value");
+
         }
 
 
         [Then(@"I click Add button")]
         public void ThenIClickAddButton()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "add button", "just_click");
+
         }
 
         [Then(@"I should be able to add new language successfully with '([^']*)' & '([^']*)'")]
-        public void ThenIShouldBeAbleToAddNewLanguageSuccessfullyWith(string eng, string p1)
+        public void ThenIShouldBeAbleToAddNewLanguageSuccessfullyWith(string language, string level)
         {
-            throw new PendingStepException();
+            profilePageObj.AssertPopUp(_driver, "added", "Fail to view success message!");
+
+            profilePageObj.AssertDisplayTxt(_driver, "language", _languageModel.Language, "Fail to add new language!");
+
+            profilePageObj.AssertDisplayTxt(_driver, "language level", _languageModel.Level, "Fail to add new language level!");
         }
 
         //Add language with invalid data
         [Given(@"I clean all existing record")]
         public void GivenICleanAllExistingRecord()
         {
-            throw new PendingStepException();
+            profilePageObj.CleanRecords(_driver, "language table", "language rows", "remove language btns");
         }
 
         [Then(@"I click on Add New button to activate form")]
         public void WhenIClickOnAddNewButtonToActivateForm()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "add new language", "wait_click");
+
         }
 
         [Then(@"I enter invalid data as '([^']*)' and select level as '([^']*)'")]
-        public void ThenIEnterInvalidDataAsAndSelectLevelAs(string p0, string p1)
+        public void ThenIEnterInvalidDataAsAndSelectLevelAs(string language, string level)
         {
-            throw new PendingStepException();
+            _languageModel.Language = language;
+            _languageModel.Level = level;
+
+            profilePageObj.EnterTxtLangOrSkill(_driver, "language textbox", "level dropdown", language, level, "by value");
         }
 
         [Then(@"I try to click Add button")]
         public void ThenITryToClickAddButton()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "add button", "just_click");
         }
 
-        [Then(@"I should not be able to add new language")]
-        public void ThenIShouldNotBeAbleToAddNewLanguage()
+        [Then(@"I should not be able to add new language with '([^']*)'")]
+        public void ThenIShouldNotBeAbleToAddNewLanguageWith(string language)
         {
-            throw new PendingStepException();
+            profilePageObj.AssertDisplayTxt(_driver, "language", _languageModel.Language, "Fail to avalidate data");
         }
+
 
         //Add language without data
         [When(@"I click on Add New button to activate language form")]
         public void WhenIClickOnAddNewButtonToActivateLanguageForm()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "add new language", "wait_click");
+
         }
 
         [Then(@"I try to click Add button to add new record")]
         public void ThenITryToClickAddButtonToAddNewRecord()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "add button", "wait_click");
         }
 
         [Then(@"I should not be able to add language and see warning message")]
         public void ThenIShouldNotBeAbleToAddLanguageAndSeeWarningMessage()
         {
-            throw new PendingStepException();
+            
+            profilePageObj.AssertPopUp(_driver, "Please enter", "Fail to view warning message!");
+
         }
 
         //Edit language
         [When(@"I click on edit icon button")]
         public void WhenIClickOnEditIconButton()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "edit language btn", "wait_click");
+
         }
 
         [Then(@"I update language as '([^']*)' and select level as '([^']*)'")]
-        public void WhenIUpdateLanguageAsAndSelectLevelAs(string vn, string fluent)
+        public void WhenIUpdateLanguageAsAndSelectLevelAs(string language, string level)
         {
-            throw new PendingStepException();
+            _languageModel.Language = language;
+            _languageModel.Level = level;
+
+            profilePageObj.EnterTxtLangOrSkill(_driver, "language textbox", "level dropdown", _languageModel.Language, _languageModel.Level, "by value");
+
         }
 
         [Then(@"I click Update button")]
         public void ThenIClickUpdateButton()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "update button", "just_click");
+
         }
 
         [Then(@"I should be able to update language successfully with '([^']*)' & '([^']*)'")]
-        public void ThenIShouldBeAbleToUpdateLanguageSuccessfullyWith(string vn, string fluent)
+        public void ThenIShouldBeAbleToUpdateLanguageSuccessfullyWith(string language, string level)
         {
-            throw new PendingStepException();
+            profilePageObj.AssertPopUp(_driver, "updated", "Fail to view success message!");
+
+            profilePageObj.AssertDisplayTxt(_driver, "language", _languageModel.Language, "Fail to edit language!");
+
+            profilePageObj.AssertDisplayTxt(_driver, "language level", _languageModel.Level, "Fail to edit language level!");
         }
 
         //Edit language invalid data
         [When(@"I click on edit icon button to activate form")]
         public void WhenIClickOnEditIconButtonToActivateForm()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "edit language btn", "wait_click");
+
         }
 
         [Then(@"I update language '([^']*)' and select level  '([^']*)'")]
-        public void WhenIUpdateLanguageAndSelectLevel(string p0, string test)
+        public void WhenIUpdateLanguageAndSelectLevel(string language, string level)
         {
-            throw new PendingStepException();
+            _languageModel.Language = language;
+            _languageModel.Level = level;
+
+            profilePageObj.EnterTxtLangOrSkill(_driver, "language textbox", "level dropdown", _languageModel.Language, _languageModel.Level, "by value");
+
         }
 
         [Then(@"I try click Update button")]
         public void ThenITryClickUpdateButton()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "update button", "just_click");
         }
 
-        [Then(@"I should not be able to update language")]
-        public void ThenIShouldNotBeAbleToUpdateLanguage()
+        [Then(@"I should not be able to update language with '([^']*)'")]
+        public void ThenIShouldNotBeAbleToUpdateLanguageWith(string language)
         {
-            throw new PendingStepException();
+            profilePageObj.AssertDisplayTxt(_driver, "language", _languageModel.Language, "Fail to validate data");
+
         }
 
-        //Edit language without data
+
+        //Edit language same data
         [When(@"I click on edit icon button to activate language form")]
         public void WhenIClickOnEditIconButtonToActivateLanguageForm()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "edit language btn", "wait_click");
+
         }
 
         [Then(@"I try click Update button to update")]
         public void ThenITryClickUpdateButtonToUpdate()
         {
-            throw new PendingStepException();
+            profilePageObj.ClickSpecific(_driver, "update button", "just_click");
+
         }
 
         [Then(@"I should not be able to update language and see warning message")]
         public void ThenIShouldNotBeAbleToUpdateLanguageAndSeeWarningMessage()
         {
-            throw new PendingStepException();
+            profilePageObj.AssertPopUp(_driver, "already added", "Fail to view warning message!");
         }
+
+        //Edit language without data
+        [When(@"I click on edit icon to activate language form")]
+        public void WhenIClickOnEditIconToActivateLanguageForm()
+        {
+            profilePageObj.ClickSpecific(_driver, "edit language btn", "wait_click");
+        }
+
+        [Then(@"I clear existing language and level")]
+        public void ThenIClearExistingLanguageAndLevel()
+        {
+            profilePageObj.ClearTextBox(_driver, "language textbox");
+
+            profilePageObj.SelectOption(_driver, "level dropdown", "Language Level", "by text");
+        }
+
+        [Then(@"I try click Update to update record")]
+        public void ThenITryClickUpdateToUpdateRecord()
+        {
+            profilePageObj.ClickSpecific(_driver, "update button", "just_click");
+        }
+
+        [Then(@"should not allow to update language and see warning message")]
+        public void ThenShouldNotAllowToUpdateLanguageAndSeeWarningMessage()
+        {
+            profilePageObj.AssertPopUp(_driver, "enter language and level", "Fail to view warning message!");
+        }
+
 
         //Delete language
         [When(@"I click on delete icon button")]
@@ -179,7 +256,7 @@ namespace SpecFlowProjectMars.StepDefinitions
         [Then(@"I should be able to delete language successfully")]
         public void ThenIShouldBeAbleToDeleteLanguageSuccessfully()
         {
-            throw new PendingStepException();
+            profilePageObj.AssertPopUp(_driver, "deleted", "Fail remove language!");
         }
     }
 }
